f5ad027b339f3d1b05f4fca31ade2275e57014c2
nadman67 <brettaj@gmail.com>
2019-05-21 08:39:30 -0500

    Update windows_main.tf

--- windows_main.tf
+++ windows_main.tf
@@ -133,7 +133,8 @@ resource "azurerm_storage_account" "mystorageaccount" {
         environment = "Terraform Demo"
     }
 }
-resource "azurerm_virtual_machine" "myterraformvm" {
+resource "azurerm_virtual_machine" "myterraformwindowsvm" {
+    count = 0
     name                  = "${var.virtual_machine_name}"
     location              = "eastus"
     resource_group_name   = "${azurerm_resource_group.myterraformgroup.name}"
@@ -162,6 +163,48 @@ resource "azurerm_virtual_machine" "myterraformvm" {
 
     os_profile_windows_config {
 	provision_vm_agent = true
+    }
+  
+    boot_diagnostics {
+        enabled     = "true"
+        storage_uri = "${azurerm_storage_account.mystorageaccount.primary_blob_endpoint}"
+    }
+
+    tags {
+        environment = "Terraform Demo"
+    }
+}
+
+resource "azurerm_virtual_machine" "myterraformlinuxvm" {
+    count = 1
+    name                  = "${var.virtual_machine_name}"
+    location              = "eastus"
+    resource_group_name   = "${azurerm_resource_group.myterraformgroup.name}"
+    network_interface_ids = ["${azurerm_network_interface.myterraformnic.id}"]
+    vm_size               = "Standard_DS1_v2"
+
+    storage_os_disk {
+        name              = "${var.storage_os_disk_name}"
+        caching           = "ReadWrite"
+        create_option     = "FromImage"
+        managed_disk_type = "Premium_LRS"
+    }
+
+    storage_image_reference {
+        publisher = "${var.image_reference_publisher}"
+        offer     = "${var.image_reference_offer}"
+        sku       = "${var.image_reference_sku}"
+        version   = "latest"
+    }
+
+    os_profile {
+        computer_name  = "${var.os_profile_computer_name}"
+        admin_username = "${var.os_profile_admin_username}"
+        admin_password ="${var.os_profile_admin_password}"    
+    }   
+
+    os_profile_linux_config {
+        disable_password_authentication = "false"
     }
   
     boot_diagnostics {
